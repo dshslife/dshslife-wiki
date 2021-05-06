5f13ba164967b6855c29e4359b1bf1015720b0e1
rzbsys <75260489+rzbsys@users.noreply.github.com>
2021-05-06 14:16:25 +0900

    Update login.py
    구글 로그인 버튼 추가

--- route/login.py
+++ route/login.py
@@ -60,7 +60,7 @@ def login_2(conn):
                         <hr class=\"main_hr\">
                         ''' + http_warring() + '''
                     </form>
-                    <a href="/oauth_login"><img src="static/login.png" alt="login_picture" ></a>
+                    <a href="/oauth_login"><button style="border-radius:5px; width: 180px; background-color: rgb(247, 247, 247); height: 50px; border: 0.1px solid rgb(230, 230, 230); line-height: 30px; padding: 10px;   box-shadow: 0px 0px 20px -20px black; font-weight:700; font-size:15px;"><img src="https://i.pinimg.com/originals/39/21/6d/39216d73519bca962bd4a01f3e8f4a4b.png" alt="구글 로고" style="width: 30px;  float: left;">구글 로그인</button></a>
                     ''',
             menu = [['user', load_lang('return')]]
         ))
